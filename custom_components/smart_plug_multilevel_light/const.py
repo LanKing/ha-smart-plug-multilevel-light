3480b9572d4fb9e9ea9ca125dedb00fec9888be1
Roman Lankin <LanKing@users.noreply.github.com>
2026-08-05 23:16:21 +0200

    Add integration constants

---
+++ custom_components/smart_plug_multilevel_light/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "smart_plug_multilevel_light"
+PLATFORMS = ["light"]
+
+CONF_OUTLET = "outlet"
+CONF_POWER_SENSOR = "power_sensor"
+CONF_CURRENT_SENSOR = "current_sensor"
+CONF_POWER_THRESHOLD = "power_threshold"
+CONF_POWER_CYCLE_DELAY = "power_cycle_delay"
+CONF_MODES = "modes"
+
+MODE_NAME = "name"
+MODE_CURRENT = "current"
+MODE_BRIGHTNESS = "brightness"
