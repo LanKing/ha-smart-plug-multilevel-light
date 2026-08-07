cd12a51e5239016880399f94595f08b4016c4f89
Roman Lankin <LanKing@users.noreply.github.com>
2026-08-07 16:20:40 +0200

    Use current sensor for lamp state

--- custom_components/smart_plug_multilevel_light/const.py
+++ custom_components/smart_plug_multilevel_light/const.py
@@ -2,9 +2,8 @@ DOMAIN = "smart_plug_multilevel_light"
 PLATFORMS = ["light"]
 
 CONF_OUTLET = "outlet"
-CONF_POWER_SENSOR = "power_sensor"
 CONF_CURRENT_SENSOR = "current_sensor"
-CONF_POWER_THRESHOLD = "power_threshold"
+CONF_OFF_CURRENT_THRESHOLD = "off_current_threshold"
 CONF_POWER_CYCLE_DELAY = "power_cycle_delay"
 CONF_MODES = "modes"
 
