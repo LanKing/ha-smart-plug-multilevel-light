936cc33ac073f96c8ce2948dca9ee3274bd80964
Roman Lankin <LanKing@users.noreply.github.com>
2026-08-10 23:52:12 +0200

    Switch history voting from current to power

--- custom_components/smart_plug_multilevel_light/const.py
+++ custom_components/smart_plug_multilevel_light/const.py
@@ -2,12 +2,12 @@ DOMAIN = "smart_plug_multilevel_light"
 PLATFORMS = ["light"]
 
 CONF_OUTLET = "outlet"
-CONF_CURRENT_SENSOR = "current_sensor"
+CONF_POWER_SENSOR = "power_sensor"
 CONF_POWER_CYCLE_DELAY = "power_cycle_delay"
-CONF_CURRENT_HISTORY_SAMPLES = "current_history_samples"
+CONF_POWER_HISTORY_SAMPLES = "power_history_samples"
 CONF_ROUND_BRIGHTNESS_TO_5 = "round_brightness_to_5"
 CONF_MODES = "modes"
 
 MODE_NAME = "name"
-MODE_CURRENT = "current"
+MODE_POWER = "power"
 MODE_BRIGHTNESS = "brightness"
