0029aab6657c085b03364d188dc62c58ea430163
Roman Lankin <LanKing@users.noreply.github.com>
2026-08-10 23:45:25 +0200

    Replace stability filter with current history voting

--- custom_components/smart_plug_multilevel_light/const.py
+++ custom_components/smart_plug_multilevel_light/const.py
@@ -3,9 +3,8 @@ PLATFORMS = ["light"]
 
 CONF_OUTLET = "outlet"
 CONF_CURRENT_SENSOR = "current_sensor"
-CONF_OFF_CURRENT_THRESHOLD = "off_current_threshold"
 CONF_POWER_CYCLE_DELAY = "power_cycle_delay"
-CONF_CURRENT_STABILITY_SAMPLES = "current_stability_samples"
+CONF_CURRENT_HISTORY_SAMPLES = "current_history_samples"
 CONF_ROUND_BRIGHTNESS_TO_5 = "round_brightness_to_5"
 CONF_MODES = "modes"
 
